8fc22a3bc3e4c27e96e0ac44657b0fd963185738
Serhat <ss99@tuta.io>
2020-09-01 22:17:35 +0300

    Filter error fixed

--- src/main/java/dev/serhats/hoodie/filter/Filter.java
+++ src/main/java/dev/serhats/hoodie/filter/Filter.java
@@ -2,6 +2,8 @@ package dev.serhats.hoodie.filter;
 
 import org.springframework.stereotype.Component;
 
+@java.lang.annotation.Target({java.lang.annotation.ElementType.TYPE})
+@java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
 @Component
 public @interface Filter {
 }
