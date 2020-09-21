5376846c6cee4c96599124cb2e97fe3b15f7df21
Serhat <ss99@tuta.io>
2020-09-22 00:57:43 +0300

    Constructor added to geolocation dto

--- src/main/java/dev/serhats/hoodie/dto/Geolocation.java
+++ src/main/java/dev/serhats/hoodie/dto/Geolocation.java
@@ -4,6 +4,11 @@ import lombok.Data;
 
 @Data
 public class Geolocation {
-    private double longitude;
     private double latitude;
+    private double longitude;
+
+    public Geolocation(double latitude,double longitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
 }
